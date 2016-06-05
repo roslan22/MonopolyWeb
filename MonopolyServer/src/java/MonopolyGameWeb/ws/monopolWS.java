dfee421d85d5bb05e46be528827f4f371b3d6790
AvielNiego <avielniego@gmail.com>
2016-06-06 00:32:44 +0300

    Some bug fixes

--- MonopolyServer/src/java/MonopolyGameWeb/ws/monopolWS.java
+++ MonopolyServer/src/java/MonopolyGameWeb/ws/monopolWS.java
@@ -10,12 +10,15 @@ import MonopolyGameWeb.logic.engine.MonopolyEngine;
 import java.util.ArrayList;
 import java.util.Arrays;
 import javax.jws.WebService;
+import ws.monopoly.DuplicateGameName;
 import ws.monopoly.Event;
 import ws.monopoly.GameDoesNotExists;
 import ws.monopoly.GameDoesNotExists_Exception;
 import ws.monopoly.GameStatus;
 import ws.monopoly.InvalidParameters;
 import ws.monopoly.InvalidParameters_Exception;
+
+import static com.sun.xml.ws.tx.common.TxFault.InvalidParameters;
 
 /**
  *
@@ -27,16 +30,11 @@ public class monopolWS {
     private Engine engine;
     public static String DEFAULT_XML_PATH = "configs/monopoly_config.xml";
     public static String DEFAULT_XSD_PATH = "configs/monopoly_config.xsd";
-
-    private Engine getEngine()
-    {
-        if (engine == null)
-            engine = new MonopolyEngine();
-        return engine;
-    }
     
     public java.util.List<Event> getEvents(int eventId, int playerId) throws ws.monopoly.InvalidParameters_Exception {
-        return getEngine().getEvents(playerId, eventId);
+        if (engine == null)
+            throw new InvalidParameters_Exception("No games", new InvalidParameters());
+        return engine.getEvents(playerId, eventId);
     }
 
     public java.lang.String getBoardSchema() {
@@ -49,23 +47,17 @@ public class monopolWS {
 
     public void createGame(int computerizedPlayers, int humanPlayers, java.lang.String name) 
             throws ws.monopoly.DuplicateGameName_Exception, ws.monopoly.InvalidParameters_Exception {
+        if(engine != null && engine.getGameStatus() != GameStatus.FINISHED)
+            throw new ws.monopoly.DuplicateGameName_Exception("Game already exists", new DuplicateGameName());
+        
         engine = new MonopolyEngine();
-   
         validatePlayersNumber(computerizedPlayers, humanPlayers);
-        if(!engine.getGameName().equals(name))
-        {
-            startGame(name, computerizedPlayers, humanPlayers);
-        }
-        else
-        {
-           throw new ws.monopoly.
-                    DuplicateGameName_Exception("Game with name:" + name + "already exist", null);        
-        }
+        startGame(name, computerizedPlayers, humanPlayers);
     }
 
     public void startGame(String name, int computerizedPlayers, int humanPlayers) {
         tryToLoadBoardFromXML();
-        getEngine().createGame(name, computerizedPlayers, humanPlayers);
+        engine.createGame(name, computerizedPlayers, humanPlayers);
     }
     
     private void validatePlayersNumber(int computerizedPlayers, int humanPlayers) throws InvalidParameters_Exception
@@ -75,39 +67,45 @@ public class monopolWS {
     }
 
     public ws.monopoly.GameDetails getGameDetails(java.lang.String gameName) throws GameDoesNotExists_Exception {
-        if (getEngine().getGameName().equals(gameName))
-            return getEngine().getGameDetails();
+        if (engine != null && engine.getGameName().equals(gameName))
+            return engine.getGameDetails();
         throw new GameDoesNotExists_Exception("No such a game name " + gameName, new GameDoesNotExists());
     }
 
     public java.util.List<java.lang.String> getWaitingGames() {
-        return getEngine().isGameWaiting() ? Arrays.asList(getEngine().getGameName()) : new ArrayList<>();
+        return (engine != null && engine.isGameWaiting()) ? Arrays.asList(engine.getGameName()) : new ArrayList<>();
     }
 
     public int joinGame(java.lang.String gameName, java.lang.String playerName) 
             throws ws.monopoly.InvalidParameters_Exception, ws.monopoly.GameDoesNotExists_Exception {
-        if (getEngine().getGameName().equals(gameName) && getEngine().getGameStatus() == GameStatus.WAITING)
-            return getEngine().joinGame(gameName, playerName);
-        if (getEngine().getGameStatus() != GameStatus.WAITING)
+        if (engine != null && engine.getGameName().equals(gameName) && engine.getGameStatus() == GameStatus.WAITING)
+            return engine.joinGame(gameName, playerName);
+        if (engine != null && engine.getGameStatus() != GameStatus.WAITING)
             throw new InvalidParameters_Exception("Game has already stated", new InvalidParameters());
         throw new GameDoesNotExists_Exception("Game does not exists", new GameDoesNotExists());
     }
 
     public ws.monopoly.PlayerDetails getPlayerDetails(int playerId) 
             throws ws.monopoly.GameDoesNotExists_Exception, ws.monopoly.InvalidParameters_Exception {
-        return getEngine().getPlayerDetails(playerId);
+        if (engine == null)
+            throw new GameDoesNotExists_Exception("No game", new GameDoesNotExists());
+        return engine.getPlayerDetails(playerId);
     }
 
     public void buy(int arg0, int arg1, boolean arg2) {
-        getEngine().buy(arg1, arg1, arg2);
+        if (engine == null)
+            return;
+        engine.buy(arg1, arg1, arg2);
     }
 
     public void resign(int playerId) throws ws.monopoly.InvalidParameters_Exception {
-        getEngine().resign(playerId);
+        if (engine == null)
+            throw new InvalidParameters_Exception("No game", new InvalidParameters());
+        engine.resign(playerId);
     }
 
     public java.util.List<ws.monopoly.PlayerDetails> getPlayersDetails(java.lang.String gameName) throws ws.monopoly.GameDoesNotExists_Exception {
-        if (engine.getGameName().equals(gameName))
+        if (engine != null && engine.getGameName().equals(gameName))
             return engine.getPlayersDetails();
         throw new GameDoesNotExists_Exception("Game does not exists", new GameDoesNotExists());
     }
@@ -117,7 +115,7 @@ public class monopolWS {
         try {
             XmlMonopolyInitReader monopolyInitReader = XmlMonopolyInitReader.getInstance(xmlPath);
             monopolyInitReader.read();
-            getEngine().initializeBoard(monopolyInitReader);
+            engine.initializeBoard(monopolyInitReader);
             //view.setDrawables(monopolyInitReader.getDrawables());
         } catch (Exception exc) {
             System.out.println("trying to load again" + exc.getMessage());
